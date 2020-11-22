a1a4a4014ee400eeac735f288d586fdc63803a79
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-22 18:27:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  super_array7 = ["wow", "pow", "zoink", "arrays!"]
+  super_array7[0] 
 end
