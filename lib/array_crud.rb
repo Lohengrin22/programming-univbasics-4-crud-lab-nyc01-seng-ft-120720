74e9b586c930cd133d9f7a1078be8a0f00e78ed6
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-22 18:19:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   even_newer_array = [1, 2, 3, 4]
-  even_newer_array.push(7)
+  even_newer_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
