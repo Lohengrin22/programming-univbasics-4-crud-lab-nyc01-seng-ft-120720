518583dbe2120860dc786a2f4fff3bcdf98c0c0e
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-22 18:23:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  super_array3 = ["wow", "pow", "zoink", "arrays!"]
+  super_array3.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
