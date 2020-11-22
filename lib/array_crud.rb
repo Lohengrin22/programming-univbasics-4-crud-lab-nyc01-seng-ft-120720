ae65e725495d099d094a016a238910d912435aa9
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-22 18:24:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  super_array4 = ["am", "pow", "zoink", "arrays!"]
+  super_array4.shift(0) 
 end
 
 def retrieve_first_element_from_array(array)
