fc150d8e4483b4ef6ff7cf1dabc1de29dc2c624e
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-22 18:24:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   super_array4 = ["am", "pow", "zoink", "arrays!"]
-  super_array4.shift(0) 
+  super_array4.shift 
 end
 
 def retrieve_first_element_from_array(array)
